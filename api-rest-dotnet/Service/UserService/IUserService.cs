4cfdad5c3468a4be49d10345241252477c7e2d51
Thiago Terra <thiagoterradev@gmail.com>
2025-10-09 20:15:03 -0300

    feat: add IUserService interface with CRUD and deactivation methods

---
+++ api-rest-dotnet/Service/UserService/IUserService.cs
@@ -0,0 +1,15 @@
+
+using api_rest_dotnet.Models;
+
+namespace api_rest_dotnet.Service.UserService
+{
+  public interface IUserService
+  {
+    Task<ServiceResponse<List<UserModel>>> GetUsers();
+    Task<ServiceResponse<List<UserModel>>> CreateUser(UserModel newUser);
+    Task<ServiceResponse<UserModel>> GetUserById(int id);
+    Task<ServiceResponse<List<UserModel>>> UpdateUser(UserModel updatedUser);
+    Task<ServiceResponse<List<UserModel>>> DeleteUser(int id);
+    Task<ServiceResponse<List<UserModel>>> DeactivateUser(int id);
+  }
+}
