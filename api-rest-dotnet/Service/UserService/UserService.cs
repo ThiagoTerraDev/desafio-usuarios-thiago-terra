55445bb0c9696415065dad56638fb5d34a93dbe0
Thiago Terra <thiagoterradev@gmail.com>
2025-10-09 20:15:53 -0300

    feat: implement UserService with GetUsers method

---
+++ api-rest-dotnet/Service/UserService/UserService.cs
@@ -0,0 +1,59 @@
+using api_rest_dotnet.DataContext;
+using api_rest_dotnet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_rest_dotnet.Service.UserService
+{
+  public class UserService : IUserService
+  { 
+    private readonly ApplicationDbContext _context;
+    public UserService(ApplicationDbContext context)
+    {
+      _context = context;
+    }
+    public async Task<ServiceResponse<List<UserModel>>> GetUsers()
+    {
+      ServiceResponse<List<UserModel>> serviceResponse = new ServiceResponse<List<UserModel>>();
+
+      try {
+        serviceResponse.Data = await _context.Users.ToListAsync();
+
+        if(serviceResponse.Data.Count == 0)
+        {
+          serviceResponse.Message = "No users found!";
+        }
+      } catch(Exception ex)
+      {
+        serviceResponse.Message = ex.Message;
+        serviceResponse.Success = false;
+      }
+
+      return serviceResponse;
+    }
+
+    public Task<ServiceResponse<List<UserModel>>> CreateUser(UserModel newUser)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<UserModel>> GetUserById(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<List<UserModel>>> UpdateUser(UserModel updatedUser)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<List<UserModel>>> DeleteUser(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<List<UserModel>>> DeactivateUser(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
