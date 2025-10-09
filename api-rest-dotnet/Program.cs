b1421ef67e787b56c009d9c8078bcfff7776e9ad
Thiago Terra <thiagoterradev@gmail.com>
2025-10-09 20:22:40 -0300

    feat: configure dependency injection and enable controllers

--- api-rest-dotnet/Program.cs
+++ api-rest-dotnet/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using api_rest_dotnet.DataContext;
+using api_rest_dotnet.Service.UserService;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
 {
@@ -20,5 +24,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
