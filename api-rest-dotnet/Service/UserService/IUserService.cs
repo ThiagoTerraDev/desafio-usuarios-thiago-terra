4cb7964f0d068abb35c080e877630d3531a145b9
Thiago Terra <thiagoterradev@gmail.com>
2025-10-09 23:17:41 -0300

    refactor: change CreateUser return type to single user

--- api-rest-dotnet/Service/UserService/IUserService.cs
+++ api-rest-dotnet/Service/UserService/IUserService.cs
@@ -6,7 +6,7 @@ namespace api_rest_dotnet.Service.UserService
   public interface IUserService
   {
     Task<ServiceResponse<List<UserModel>>> GetUsers();
-    Task<ServiceResponse<List<UserModel>>> CreateUser(UserModel newUser);
+    Task<ServiceResponse<UserModel>> CreateUser(UserModel newUser);
     Task<ServiceResponse<UserModel>> GetUserById(int id);
     Task<ServiceResponse<List<UserModel>>> UpdateUser(UserModel updatedUser);
     Task<ServiceResponse<List<UserModel>>> DeleteUser(int id);
