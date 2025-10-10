7084c07e7cf2edad4e14e14e42184cf3eac7bc97
Thiago Terra <thiagoterradev@gmail.com>
2025-10-10 18:59:27 -0300

    refactor: change UpdateUser return type to single user

--- api-rest-dotnet/Service/UserService/IUserService.cs
+++ api-rest-dotnet/Service/UserService/IUserService.cs
@@ -8,7 +8,7 @@ namespace api_rest_dotnet.Service.UserService
     Task<ServiceResponse<List<UserModel>>> GetUsers();
     Task<ServiceResponse<UserModel>> CreateUser(UserModel newUser);
     Task<ServiceResponse<UserModel>> GetUserById(int id);
-    Task<ServiceResponse<List<UserModel>>> UpdateUser(UserModel updatedUser);
+    Task<ServiceResponse<UserModel>> UpdateUser(UserModel updatedUser);
     Task<ServiceResponse<List<UserModel>>> DeleteUser(int id);
     Task<ServiceResponse<UserModel>> DeactivateUser(int id);
   }
