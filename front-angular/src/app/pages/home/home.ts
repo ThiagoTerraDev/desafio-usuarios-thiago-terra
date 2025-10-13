3f1e85b431dfb5aab61b21649b428d52a4826184
Thiago Terra <thiagoterradev@gmail.com>
2025-10-13 20:43:52 -0300

    feat: implement user listing and real-time search

--- front-angular/src/app/pages/home/home.ts
+++ front-angular/src/app/pages/home/home.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,44 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit {
+
+  users: User[] = [];
+  allUsers: User[] = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.GetUsers().subscribe({
+      next: (response) => {
+        const { data } = response;
+
+        data.map(user => {
+          user.createdAt = new Date(user.createdAt!).toLocaleDateString('pt-BR');
+          user.updatedAt = new Date(user.updatedAt!).toLocaleDateString('pt-BR');
+        });
+
+        this.users = data;
+        this.allUsers = data;
+
+        console.log('Users fetched successfully:', this.users);
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      },
+      complete: () => {
+        console.log('Request to get users completed.');
+      }
+    });
+  }
+
+  search(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value.toLowerCase();
+
+    this.users = this.allUsers.filter(user =>
+      user.name.toLowerCase().includes(value)
+    );
+  }
 
 }
