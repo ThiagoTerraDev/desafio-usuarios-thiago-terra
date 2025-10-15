de60611aef38a309b25ff2f61b3b671692325611
Thiago Terra <thiagoterradev@gmail.com>
2025-10-15 00:47:55 -0300

    feat: add reusable user form component

---
+++ front-angular/src/app/components/user-form/user-form.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../../models/user';
+
+@Component({
+  selector: 'app-user-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './user-form.html',
+  styleUrl: './user-form.css'
+})
+export class UserForm implements OnInit {
+
+  @Output() onSubmit = new EventEmitter<User>();
+  @Input() btnTitle!: string;
+  @Input() btnAction!: string;
+  @Input() userData: User | null = null;
+
+  userForm!: FormGroup;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      id: new FormControl(this.userData ? this.userData?.id : 0),
+      name: new FormControl(this.userData ? this.userData?.name : '', [Validators.required]),
+      lastName: new FormControl(this.userData ? this.userData?.lastName : '', [Validators.required]),
+      department: new FormControl(this.userData ? this.userData?.department : '', [Validators.required]),
+      shift: new FormControl(this.userData ? this.userData?.shift : '', [Validators.required]),
+      active: new FormControl(this.userData ? this.userData?.active : true),
+      createdAt: new FormControl(new Date()),
+      updatedAt: new FormControl(new Date())
+    });
+  }
+
+  submit(): void {
+    this.onSubmit.emit(this.userForm.value);
+  }
+
+  goBack(): void {
+    this.router.navigate(['/']);
+  }
+
+}
