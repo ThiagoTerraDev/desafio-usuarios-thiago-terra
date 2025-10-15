9f59d6bb0c9bf1f45641f00d287702032025a889
Thiago Terra <thiagoterradev@gmail.com>
2025-10-15 00:55:15 -0300

    feat: configure routes for register and edit pages

--- front-angular/src/app/app.routes.ts
+++ front-angular/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Register } from './pages/register/register';
+import { Home } from './pages/home/home';
+import { Edit } from './pages/edit/edit';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: Home
+  },
+  {
+    path: 'register', component: Register
+  },
+  {
+    path: 'edit/:id', component: Edit
+  }
+];
