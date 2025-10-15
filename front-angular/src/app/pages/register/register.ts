f246dad219e42987ba455babd5234db2497d7b95
Thiago Terra <thiagoterradev@gmail.com>
2025-10-15 00:49:58 -0300

    feat: add user registration page

---
+++ front-angular/src/app/pages/register/register.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { UserForm } from "../../components/user-form/user-form";
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  imports: [UserForm],
+  templateUrl: './register.html',
+  styleUrl: './register.css'
+})
+export class Register {
+
+  constructor(private userService : UserService, private router: Router) { }
+
+  btnTitle: string = 'Cadastrar Usuário';
+  btnAction: string = 'Cadastrar';
+
+  createUser(user: User) {
+    this.userService.CreateUser(user).subscribe({
+      next: (response) => {
+        alert('Usuário criado com sucesso!');
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        console.error('Error creating user:', error);
+        alert('Error creating user. Please try again.');
+      },
+      complete: () => {
+        console.log('Request to create user completed.');
+      }
+    });
+  }
+}
