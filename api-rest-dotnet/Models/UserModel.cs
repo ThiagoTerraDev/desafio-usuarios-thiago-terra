05420d8b28cfd49b65ce622fe6015f4f08b5b902
Thiago Terra <thiagoterradev@gmail.com>
2025-10-19 20:12:48 -0300

    feat: add authentication fields to UserModel

--- api-rest-dotnet/Models/UserModel.cs
+++ api-rest-dotnet/Models/UserModel.cs
@@ -1,14 +1,22 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 using api_rest_dotnet.Enums;
 
 namespace api_rest_dotnet.Models
 {
+    [Index(nameof(Email), IsUnique = true)]
     public class UserModel
     {
       [Key]
       public int Id { get; set; }
       public required string Name { get; set; }
       public required string LastName { get; set; }
+      public required string Email { get; set; }
+      public required string PasswordHash { get; set; }
+      public required string PasswordSalt { get; set; }      
+      public string? ResetPasswordToken { get; set; }      
+      public DateTime? ResetPasswordTokenExpires { get; set; }
+      public DateTime TokenCreatedAt { get; set; } = DateTime.UtcNow;
       public DepartmentEnum Department { get; set; }
       public bool Active { get; set; }
       public ShiftEnum Shift { get; set; }
