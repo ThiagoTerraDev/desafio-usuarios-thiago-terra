7a7c78c721288def9bd681f1ba1e1603b009588f
Thiago Terra <thiagoterradev@gmail.com>
2025-10-21 15:37:43 -0300

    test: update UserService test configuration

--- front-angular/src/app/services/user.service.spec.ts
+++ front-angular/src/app/services/user.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideZoneChangeDetection } from '@angular/core';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(UserService);
   });
 
