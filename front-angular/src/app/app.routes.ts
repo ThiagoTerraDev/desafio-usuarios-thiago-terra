71f43b190be1f421cef9c5c6199698e8a129adbc
Thiago Terra <thiagoterradev@gmail.com>
2025-10-21 00:48:50 -0300

    feat: add forgot and reset password routes

--- front-angular/src/app/app.routes.ts
+++ front-angular/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Signup } from './pages/signup/signup';
+import { ForgotPassword } from './pages/forgot-password/forgot-password';
+import { ResetPassword } from './pages/reset-password/reset-password';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Create } from './pages/create/create';
 import { Edit } from './pages/edit/edit';
@@ -11,6 +13,8 @@ import { publicGuard } from './guards/public.guard';
 export const routes: Routes = [
   { path: 'login', component: Login, canActivate: [publicGuard] },
   { path: 'signup', component: Signup, canActivate: [publicGuard] },
+  { path: 'forgot-password', component: ForgotPassword, canActivate: [publicGuard] },
+  { path: 'reset-password', component: ResetPassword },
   { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
   { path: 'create', component: Create, canActivate: [authGuard] },
   { path: 'edit/:id', component: Edit, canActivate: [authGuard] },
